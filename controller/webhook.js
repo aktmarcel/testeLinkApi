2f62c45031b4219e7a7338f9cdec1231a8da53b5
pedro <salgadopedroh@gmail.com>
2020-08-14 00:49:45 -0300

    Nome da propriedade arrumado

--- controller/webhook.js
+++ controller/webhook.js
@@ -2,9 +2,9 @@ const Pipedrive = require('../models/Pipedrive');
 const Bling = require('../models/Bling');
 module.exports.index = async (req, res) =>{
     try {
-        const pipe = new Pipedrive().createOrUpdate(req.body.data);
+        const pipe = new Pipedrive().createOrUpdate(req.body.current);
         console.log(JSON.stringify(pipe));
-        const bling = new Bling(req.body.data);
+        const bling = new Bling(req.body.current);
         const response = await bling.insertOrder();
         res.status(201).send();
         
